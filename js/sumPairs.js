f58554fc39d4e4f177a335f0c0fcea694a3e1ce2
Garrett Adams <garrett.adams52@gmail.com>
2022-05-30 15:25:44 -0400

    finished js

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,17 @@
-exports.sumPairs = function() {
 
-};
+function sumPairs(arr, num){
+    ans = []
+    for(let i = 0; i < arr.length; i++){
+        for(let j = arr.length-1; j > i; j--){
+            //console.log(arr[i], arr[j])
+            if(arr[i]+arr[j]===num)
+                ans.push([arr[i],arr[j]])
+        }
+    }
+
+    return ans
+}
+
+
+console.log(sumPairs([1,2,3,4,5], 7))
+//exports.sumPairs = {sumPairs}
